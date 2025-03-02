dd4e6fcfbb80a8d896a21a88ac7d75a32df14bcc
Yune Cho <fusma159@naver.com>
2025-03-02 20:09:27 +0900

    Fix iOS screen rotation problem

---
+++ AppDelegate.swift
@@ -0,0 +1,8 @@
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
+}
--- EmotionFeedbackApp.swift
+++ EmotionFeedbackApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct EmotionFeedbackApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     @State private var appStateManager = AppStateManager()
     
     var body: some Scene {
